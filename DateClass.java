91d0723d3d6b5cb1b513f35afae152300931a5c9
DanielSzpunar <danielszpunar@gmail.com>
2017-10-16 14:33:16 -0400

    Date class

---
+++ DateClass.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class DateClass {
+  public static void main(String[] args) {
+    Date date = new Date();
+    System.out.println(date);
+    System.out.println("Is the current date and time.");
+  }
+}
